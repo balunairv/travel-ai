5366057dd3abee3bf7b31ef391d55698c40368e5
Balu <baluprasadam@hmail.com>
2025-04-28 00:29:39 +0530

    build

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/travel-ai/',
+  // base: '/travel-ai/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
